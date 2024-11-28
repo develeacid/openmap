a292d7214c5162d765cfdc338923c14c70d7c38c
eleacid <eleacid@prueba.com>
2024-11-28 14:47:33 -0600

    ejemplo de capa regional

--- resources/views/mapcomponent.blade.php
+++ resources/views/mapcomponent.blade.php
@@ -2,6 +2,6 @@
 
 @section('content')
     <h1>Bienvenido a mi componente de Open Layer</h1>
-    <x-open-map mapId="maping" geoJsonUrl="{{ asset('GeoJson/municipales/prueba.geojson') }}" :center="[-100, 20]" :zoom="4" />
+    <x-open-map mapId="maping" geoJsonUrl="{{ asset('GeoJson/municipales/001.geojson') }}" :center="[-100, 20]" :zoom="4" />
     <x-open-map mapId="maping" geoJsonUrl="{{ asset('GeoJson/regionales/PAPALOAPAM.geojson') }}" :center="[-100, 20]" :zoom="4" />
 @endsection
