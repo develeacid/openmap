1f5576a44a6524eb2434571b801dc0da094c9ff8
eleacid <eleacid@prueba.com>
2024-12-03 13:31:23 -0600

    opemmap-info-div

--- resources/views/components/open-map.blade.php
+++ resources/views/components/open-map.blade.php
@@ -7,8 +7,11 @@
 
     El mapa se inicializa usando la función 'inicializarMapa' que se ejecuta cuando el DOM ha sido completamente cargado.
 --}}
+<div id="openmap-container" style="position: relative;"> 
+    
+    <div id="map-{{ $mapId }}" style="height: 400px;" ></div>  {{-- Contenedor del mapa con un ID único generado a partir de $mapId. La altura del mapa está fijada a 400px. --}}
 
-<div id="map-{{ $mapId }}" style="height: 400px;" ></div>  {{-- Contenedor del mapa con un ID único generado a partir de $mapId. La altura del mapa está fijada a 400px. --}}
+</div>
 
 <script>
     // Cuando el DOM esté completamente cargado, se ejecuta el siguiente script para inicializar el mapa.
